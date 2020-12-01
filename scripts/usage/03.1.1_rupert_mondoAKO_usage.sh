6ac0a0b3d305cfd311efac7856c3e72a42219df5
Gaurav Jain <gauravj49@gmail.com>
2020-12-01 15:18:41 +0100

    Custom peak calling using cutoff-analysis and DBA analysis using diffbind

--- scripts/usage/03.1.1_rupert_mondoAKO_usage.sh
+++ scripts/usage/03.1.1_rupert_mondoAKO_usage.sh
@@ -146,9 +146,6 @@ macs2 callpeak -f BAM --seed=39751 -g hs  -p ${pvalue} --keep-dup auto  --cutoff
 macs2 callpeak -f BAM --seed=39751 -g hs  -p ${pvalue} --keep-dup auto  --cutoff-analysis -t ${bamDir}/Cas9-RNAPolymeraseII_R1.mLb.clN.sorted.bam ${bamDir}/Cas9-RNAPolymeraseII_R2.mLb.clN.sorted.bam -c ${bamDir}/Cas9-input_R1.mLb.clN.sorted.bam ${bamDir}/Cas9-input_R2.mLb.clN.sorted.bam -n Cas9-RNAPolymeraseII_over_input_p${pd} --outdir ${peaksOutDir}
 
 
-
-
-
 pd="0_00025"; pvalue="${pd//_/.}"; peaksOutDir=${customPeaksDir}/customThreshold
 # Cas9-MondoA/IgG
 macs2 callpeak -f BAM --seed=39751 -g hs  -p ${pvalue} --keep-dup auto  --cutoff-analysis -t ${bamDir}/Cas9-MondoA_R1.mLb.clN.sorted.bam ${bamDir}/Cas9-MondoA_R2.mLb.clN.sorted.bam -c ${bamDir}/Cas9-IgG_R1.mLb.clN.sorted.bam ${bamDir}/Cas9-IgG_R2.mLb.clN.sorted.bam -n Cas9-MondoA_over_IgG_p${pd} --outdir ${peaksOutDir}
@@ -156,3 +153,153 @@ macs2 callpeak -f BAM --seed=39751 -g hs  -p ${pvalue} --keep-dup auto  --cutoff
 pd="0_001"; pvalue="${pd//_/.}"; peaksOutDir=${customPeaksDir}/customThreshold
 # Cas9-MondoA/input
 macs2 callpeak -f BAM --seed=39751 -g hs  -p ${pvalue} --keep-dup auto  --cutoff-analysis -t ${bamDir}/Cas9-MondoA_R1.mLb.clN.sorted.bam ${bamDir}/Cas9-MondoA_R2.mLb.clN.sorted.bam -c ${bamDir}/Cas9-input_R1.mLb.clN.sorted.bam ${bamDir}/Cas9-input_R2.mLb.clN.sorted.bam -n Cas9-MondoA_over_input_p${pd} --outdir ${peaksOutDir}
+
+# Annotate using chipseeker
+peaksOutDir=${customPeaksDir}/customThreshold
+for peaksInputFile in $(ls ${peaksOutDir}/*.narrowPeak)
+do
+	peaksAnnTxtFile=${peaksOutDir}/$(basename ${peaksInputFile} .narrowPeak)_annotation.txt
+	echo Rscript ${jobdir}/scripts/R_annotate_peaks_chipseeker.R -if=${peaksInputFile} -of=${peaksAnnTxtFile} -sp=${species} -mf
+	Rscript ${jobdir}/scripts/R_annotate_peaks_chipseeker.R -if=${peaksInputFile} -of=${peaksAnnTxtFile} -sp=${species} -mf
+done
+
+# Call individual peaks for replicates individually
+pd="0_008"; pvalue="${pd//_/.}"; peaksOutDir=${customPeaksDir}/individualCustomPeaks
+# myc/input
+macs2 callpeak -f BAM --seed=39751 -g hs  -p ${pvalue} --keep-dup auto  --cutoff-analysis -t ${bamDir}/5B4-MYC_R1.mLb.clN.sorted.bam -c ${bamDir}/5B4-input_R1.mLb.clN.sorted.bam -n 5B4-MYC_R1_over_input_R1_p${pd} --outdir ${peaksOutDir}
+macs2 callpeak -f BAM --seed=39751 -g hs  -p ${pvalue} --keep-dup auto  --cutoff-analysis -t ${bamDir}/5B4-MYC_R2.mLb.clN.sorted.bam -c ${bamDir}/5B4-input_R2.mLb.clN.sorted.bam -n 5B4-MYC_R2_over_input_R2_p${pd} --outdir ${peaksOutDir}
+
+# Cas9-Myc/input
+macs2 callpeak -f BAM --seed=39751 -g hs  -p ${pvalue} --keep-dup auto  --cutoff-analysis -t ${bamDir}/Cas9-MYC_R1.mLb.clN.sorted.bam -c ${bamDir}/Cas9-input_R1.mLb.clN.sorted.bam -n Cas9-MYC_R1_over_input_R1_p${pd} --outdir ${peaksOutDir}
+macs2 callpeak -f BAM --seed=39751 -g hs  -p ${pvalue} --keep-dup auto  --cutoff-analysis -t ${bamDir}/Cas9-MYC_R2.mLb.clN.sorted.bam -c ${bamDir}/Cas9-input_R2.mLb.clN.sorted.bam -n Cas9-MYC_R2_over_input_R2_p${pd} --outdir ${peaksOutDir}
+
+pd="0_001"; pvalue="${pd//_/.}"; peaksOutDir=${customPeaksDir}/individualCustomPeaks
+# Cas9-MondoA/input
+macs2 callpeak -f BAM --seed=39751 -g hs  -p ${pvalue} --keep-dup auto  --cutoff-analysis -t ${bamDir}/Cas9-MondoA_R1.mLb.clN.sorted.bam -c ${bamDir}/Cas9-input_R1.mLb.clN.sorted.bam -n Cas9-MondoA_R1_over_input_R1_p${pd} --outdir ${peaksOutDir}
+macs2 callpeak -f BAM --seed=39751 -g hs  -p ${pvalue} --keep-dup auto  --cutoff-analysis -t ${bamDir}/Cas9-MondoA_R2.mLb.clN.sorted.bam -c ${bamDir}/Cas9-input_R2.mLb.clN.sorted.bam -n Cas9-MondoA_R2_over_input_R2_p${pd} --outdir ${peaksOutDir}
+
+# Copy the samplesheet to the server
+scp -r mondoAKO_SampleSheet_DiffBind.csv iws4:/media/rad/HDD1/nfchip/rupert/mondoAKO/
+
+# Run Diffbind
+# https://bioinformatics-core-shared-training.github.io/cruk-summer-school-2020/ChIPSeq/scripts/ChIP_Practical3_DiffBind.html
+R
+
+# # In case we do not have these packages
+# install.packages("tidyverse")
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#     install.packages("BiocManager")
+# BiocManager::install("DiffBind")
+
+# load required packages:
+library(DiffBind)
+library(tidyverse)
+library(rtracklayer)
+
+# 1) Step 1: Reading a peakset
+# 1.1) Read a csv file
+samples <- read.csv("/media/rad/HDD1/nfchip/rupert/mondoAKO/mondoAKO_SampleSheet_DiffBind.csv")
+# 1.2) Look at the loaded metadata
+names(samples)
+# 1.3) The peaksets are read in using the following DiffBind function, that will construct a new DBA object from the sample sheet.
+mondoAKO <- dba(sampleSheet="/media/rad/HDD1/nfchip/rupert/mondoAKO/mondoAKO_SampleSheet_DiffBind.csv")
+
+# 2) Step 2: Occupancy analysis
+dba.plotHeatmap(mondoAKO)
+
+# 3) Step 3: Counting reads
+# 3.1) Take the alignment files (BAM) and compute count information for each of the peaks/regions in the consensus set
+mondoAKO.counted <- dba.count(mondoAKO, summits=250)
+mondoAKO.counted
+# 6 Samples, 1621 sites in matrix:
+#               ID Condition Caller Intervals
+# 1     5B4-MYC_R1       MKO counts      1621
+# 2     5B4-MYC_R2       MKO counts      1621
+# 3 Cas9-MondoA_R1  MondoAKO counts      1621
+# 4 Cas9-MondoA_R2  MondoAKO counts      1621
+# 5    Cas9-MYC_R1  MondoAWT counts      1621
+# 6    Cas9-MYC_R2  MondoAWT counts      1621
+
+# 3.2) With this matrix, the samples can be re-clustered using affinity, rather than occupancy data:
+cat("\n- draw a Sample correlation plot using all 1621 consensus sites\n")
+SCRplotFile     <- paste0("/media/rad/HDD1/nfchip/rupert/mondoAKO/analysis/dba/mondoAKO_sampleCorrelation.jpg", sep='') 
+jpeg(filename=SCRplotFile, height=900, width=1000, bg="white", res=100); par(mar=c(20,4,1,1));
+dba.plotHeatmap(mondoAKO.counted)
+dev.off()
+
+# 3.3) To see how well the samples cluster with one another, we can draw a PCA plot using all 1621 consensus sites.
+cat("\n- draw a PCA plot using all 1621 consensus sites\n")
+PCAplotFile     <- paste0("/media/rad/HDD1/nfchip/rupert/mondoAKO/analysis/dba/mondoAKO_PCA.jpg", sep='') 
+jpeg(filename=PCAplotFile, height=900, width=1000, bg="white", res=100); par(mar=c(20,4,1,1));
+dba.plotPCA(mondoAKO.counted,  attributes=DBA_CONDITION, label=DBA_ID)
+dev.off()
+
+# 3.4) Open the file from terminal
+eog mondoAKO_PCA.jpg
+eog mondoAKO_sampleCorrelation.jpg
+
+# 4) Step 4: Differential binding affinity analysis
+# 4.1) 	we have to let DiffBind know how we want to group our samples. 
+# 			In our case we will group based on condition. 
+# 			This is done using the function, as follows:
+mondoAKO.counted <- dba.contrast(mondoAKO.counted, categories=DBA_CONDITION, minMembers=2)
+# 3 Contrasts:
+#     Group1 Members1   Group2 Members2
+# 1      MKO        2 MondoAKO        2
+# 2      MKO        2 MondoAWT        2
+# 3 MondoAKO        2 MondoAWT        2
+# 4.2) The main differential analysis function using DESeq2 is invoked as follows:
+mondoAKO.analysed <- dba.analyze(mondoAKO.counted)
+
+# 4.3) plots
+dba.plotHeatmap(mondoAKO.analysed, contrast=2, th=1)
+PCAplotFile     <- paste0("/media/rad/HDD1/nfchip/rupert/mondoAKO/analysis/dba/mondoAKO_binding_affinity_heatmap.jpg", sep='') 
+jpeg(filename=PCAplotFile, height=900, width=1000, bg="white", res=100); par(mar=c(20,4,1,1));
+dba.plotHeatmap(mondoAKO.analysed, ColAttributes = DBA_CONDITION, contrast=2, correlations=FALSE, th=1)
+dev.off()
+
+# 4.4) Other plots:
+# 4.4.1) MA plots
+PCAplotFile     <- paste0("/media/rad/HDD1/nfchip/rupert/mondoAKO/analysis/dba/mondoAKO_binding_affinity_MA_plot.jpg", sep='') 
+jpeg(filename=PCAplotFile, height=900, width=1000, bg="white", res=100); par(mar=c(20,4,1,1));
+dba.plotMA(mondoAKO.analysed)
+dev.off()
+# 4.4.2) Volcano plots
+PCAplotFile     <- paste0("/media/rad/HDD1/nfchip/rupert/mondoAKO/analysis/dba/mondoAKO_binding_affinity_volcano_plot.jpg", sep='') 
+jpeg(filename=PCAplotFile, height=900, width=1000, bg="white", res=100); par(mar=c(20,4,1,1));
+dba.plotVolcano(mondoAKO.analysed)
+dev.off()
+# 4.4.3) PCA plots for contrast=2
+PCAplotFile     <- paste0("/media/rad/HDD1/nfchip/rupert/mondoAKO/analysis/dba/mondoAKO_binding_affinity_PCA_MKOvsMondoAWT_plot.jpg", sep='') 
+jpeg(filename=PCAplotFile, height=900, width=1000, bg="white", res=100); par(mar=c(20,4,1,1));
+dba.plotPCA(mondoAKO.analysed, contrast=2, th=1)
+dev.off()
+# 4.4.4) # Boxplots can give us an idea about the read distribution differences between the classes 
+# - in our case the two conditions. The first two boxes show distribution of reads over all 
+# differentially bound sites; the middle two show differences on those sites where the affinity 
+# increases in Responsive and the two boxes on the right show differences where the affinity increases in Resistant samples.
+PCAplotFile     <- paste0("/media/rad/HDD1/nfchip/rupert/mondoAKO/analysis/dba/mondoAKO_binding_affinity_box_plot.jpg", sep='') 
+jpeg(filename=PCAplotFile, height=900, width=1000, bg="white", res=100); par(mar=c(20,4,1,1));
+dba.plotBox(mondoAKO.analysed)
+dev.off()
+
+# 5) Report the differentially bound peak regions, identified by DESeq2. 
+# These results files contain the genomic coordinates for all consensus sites and 
+# statistics for differential enrichment including fold-change, p-value and FDR.
+report <- dba.report(mondoAKO.analysed)
+report
+# The value columns are described below:
+# 	Chr: Chromosome of binding site
+# 	Start: Starting base position of binding site
+# 	End: End base position of binding site
+# 	Conc: mean read concentration over all the samples (the default calculation uses log2 normalized ChIP read counts with control read counts subtracted)
+# 	Conc_group1: Group 1 Concentration
+# 	Conc_group2: Group 2 Concentration
+# 	Fold: Fold difference – mean fold difference of binding affinity of group 1 over group 2 (Conc1 - Conc2)
+# 	p-valule and *FDR statistic indicating significance of difference
+# 	Before writing to file we need to convert it to a data frame so that genomic coordinates get written as columns and not GRanges.
+report.df <- as.data.frame(report)  
+write.table(report.df, "/media/rad/HDD1/nfchip/rupert/mondoAKO/analysis/dba/mondoAKO_report.csv", sep="\t", quote=F, row.names=F)
+
+# 6) 
+# PeakChrom	PeakStart	PeakEnd	PeakID	width	strand	Conc	Fold	p.value	FDR
